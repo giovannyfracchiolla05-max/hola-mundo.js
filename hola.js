e1fa2d59ebd5d97c242d912f6b352a7f5d32cd07
giovannyfracchiolla05-max <giovannyfracchiolla05@gmail.com>
2025-10-27 17:53:15 +0100

    Update hola.js

--- hola.js
+++ hola.js
@@ -1,1 +1,9 @@
 console.log("Hola, mundo");
+const age = 1995;
+let JS = {
+  name: "JavaScript",
+  language: "fronted",
+  age,
+  getAge: () => new Date(). getFullyear() - age,
+}
+console.log(JS.getAge());
